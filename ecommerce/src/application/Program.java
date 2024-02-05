f8838d0fcd68df1b0b0b951aa9a4e94f4e910f3f
aarrieche <arriechealexandre@gmail.com>
2024-02-05 18:59:42 -0300

    Class category

---
+++ ecommerce/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import domain.Category;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Category c1 = new Category(1, "aluminum");
+		System.out.println(c1);
+	}
+
+}
