e9a1943db2ab63732ac0e84e7c1aaf60f86ff5ef
Rllucena <rafael_l_lucena@hotmail.com>
2024-02-07 01:45:43 -0300

    Added user choices

--- ecommerceJava/src/main/java/application/Program.java
+++ ecommerceJava/src/main/java/application/Program.java
@@ -23,6 +23,43 @@ public class Program {
 		System.out.println(c1);
 		System.out.println("Teste");
 		
+		int option =0;
+		while (option != 6) {
+		
+			try {
+				System.out.println("Digite uma opção: 1- Listar produtos; 2- Buscar produto; 3- Cadastrar produto; 4- Atualizar produto; 5 - Excluir produto; 6 - sair;");
+				option = in.nextInt();
+			}
+			catch (NumberFormatException e) {
+				System.out.println("Erro, digite apenas números.");
+			} 
+			
+			switch (option) {
+				case 1:
+					System.out.println("1- Listar produtos:");
+				
+					break;
+				case 2:
+					System.out.println("2- Buscar produto:");
+				
+					break;
+				case 3:
+					System.out.println("3- Cadastrar produto:");
+				
+					break;
+				case 4:
+					System.out.println("4- Atualizar produto:");
+				
+					break;
+				case 5:
+					System.out.println("5 - Excluir produto:");
+				
+					break;					
+				
+				default:
+					break;
+			}
+		}	
 	}
 
 }
