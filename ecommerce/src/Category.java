e27a5ebfe4c86d8df260ba419f94e91b696e72ba
Pablo9293 <pabloagm7@gmail.com>
2024-02-08 21:31:59 -0300

    UpdateCategory

--- ecommerce/src/Category.java
+++ ecommerce/src/Category.java
@@ -1,23 +1,27 @@
 package domain;
 
-public class Category {
+import java.io.Serializable;
 
-	private int id;
-	
+public class Category implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String id;
 	private String name;
-	
 
-	public Category(int id, String name) {
-		super();
+	public Category() {
+
+	}
+
+	public Category(String id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-	public int getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
@@ -30,9 +34,33 @@ public class Category {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "Category [id=" + id + ", name=" + name + "]";
 	}
-	
-	
+
 }
